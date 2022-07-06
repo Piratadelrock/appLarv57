3b0c90d316a1583ca559a333cbbf63f9557c9d5d
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2022-07-05 19:15:45 -0500

    creamos la tabla, y la relacion entre los gastos y los reportes

--- app/ExpenseReport.php
+++ app/ExpenseReport.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExpenseReport extends Model
 {
-    
+    public function expenses(){
+        return $this->hasMany(Expense::class);
+    }
 
     
 }
